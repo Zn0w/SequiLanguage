2281f44eb2fc8b00ae31c5f7217e59962005eda8
Daniil Blagiy <blagiy.daniil@gmail.com>
2019-07-13 11:28:42 +0300

    added read_file function + tested it

--- SequiLanguage/src/sequi.cpp
+++ SequiLanguage/src/sequi.cpp
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <string>
 
-int main()
+#include "../utils/file.h"
+
+
+int main(int argc, char** argv)
 {
-	printf("Hello World!\n");
+	if (argc == 1)
+	{
+		printf("Usage: sequi <filepath>\n");
+		printf("<filepath> is the file with .sequi extension\n");
+		return 0;
+	}
+
+	std::string file_contents = read_file(argv[1]);
+
+	printf("%s\n", file_contents.c_str());
+	getchar();
 
 	return 0;
 }
---
+++ SequiLanguage/utils/file.h
@@ -0,0 +1,25 @@
+#pragma once
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+
+std::string read_file(const char* filepath)
+{
+	FILE* file = fopen(filepath, "rt");		// rt stands for 'read text'
+	fseek(file, 0, SEEK_END);				// go to the end of file
+	unsigned long length = ftell(file);		// tell the amount of passed bytes
+	char* data = (char*)malloc(length + 1);	// +1 for termination character
+	memset(data, 0, length + 1);
+	fseek(file, 0, SEEK_SET);				// go to the beggining of the file
+	fread(data, 1, length, file);
+	fclose(file);
+
+	std::string result(data);
+
+	free(data);
+
+	return result;
+}

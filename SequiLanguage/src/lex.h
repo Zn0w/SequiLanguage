2e953d96e5456b93374ef7e996d740044cb88984
Daniil Blagiy <blagiy.daniil@gmail.com>
2019-07-15 11:38:35 +0300

    working on lex analysis

--- SequiLanguage/src/lex.h
+++ SequiLanguage/src/lex.h
@@ -8,8 +8,18 @@
 #include "token.h"
 
 
+enum LexMode
+{
+	DEFAULT,
+	STRING,
+	COMMENT,
+	PRINTING
+};
+
 std::vector<Token> lex(std::string source)
 {
+	LexMode mode = DEFAULT;
+	
 	// split string by space
 	std::stringstream test(source);
 	std::string segment;
@@ -24,7 +34,109 @@ std::vector<Token> lex(std::string source)
 	
 	for (std::string token : unprocessed)
 	{
+		switch (mode)
+		{
 
+		case DEFAULT:
+		{
+			TokenType type;
+
+			// if single-character token
+			if (token.size() == 1)
+			{
+				switch (token.at(0))
+				{
+				case '(':
+					type = LEFT_PAREN;
+					break;
+				case ')':
+					type = RIGHT_PAREN;
+					break;
+				case '{':
+					type = LEFT_BRACE;
+					break;
+				case '}':
+					type = RIGHT_PAREN;
+					break;
+				case '[':
+					type = LEFT_BRACK;
+					break;
+				case ']':
+					type = RIGHT_PAREN;
+					break;
+				case ',':
+					type = COMMA;
+					break;
+				case '.':
+					type = DOT;
+					break;
+				case ';':
+					type = SEMICOLON;
+					break;
+				case ':':
+					type = COLON;
+					break;
+				case '+':
+				case '-':
+				case '*':
+				case '/':
+				case '=':
+				case '>':
+				case '<':
+					type = OPERATOR;
+					break;
+				case '"':
+					type = QUOTE;
+					// go to the string mode
+					break;
+
+				default:
+					// check if literal, if not print error
+					break;
+				}
+			}
+
+			// if keyword
+			else if (token == "var")
+				type = VAR;
+			else if (token == "func")
+				type = FUNC;
+			else if (token == "if")
+				type = IF;
+			else if (token == "else")
+				type = ELSE;
+			else if (token == "elif")
+				type = ELIF;
+			else if (token == "and")
+				type = AND;
+			else if (token == "or")
+				type = OR;
+			else if (token == "not")
+				type = NOT;
+			else if (token == "print")
+				// go to the printing mode
+				type = PRINT;
+			else if (token == "true")
+				type = TRUE;
+			else if (token == "false")
+				type = FALSE;
+			else if (token == "return")
+				type = RETURN;
+
+			// if literal
+			else if (std::stoi(token))
+				type = NUMBER;
+			// else check if valid identifier
+
+			tokens.push_back(Token(type, token));
+		} break;
+
+		case STRING :
+		{
+			
+		} break;
+
+		}
 	}
 
 	return tokens;

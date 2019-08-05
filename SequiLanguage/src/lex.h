f61e276862454d9a4362769cb4ff6be61ce9ca04
Daniil Blagiy <blagiy.daniil@gmail.com>
2019-08-05 18:31:56 +0300

    moved source code from token.h to lex.h, working on syntax tree generation

--- SequiLanguage/src/lex.h
+++ SequiLanguage/src/lex.h
@@ -6,8 +6,45 @@
 #include <vector>
 #include <cctype>
 
-#include "token.h"
 
+/*enum TokenType
+{
+KEYWORD, IDENTIFIER, NUMBER, STRING, CHAR, OPERATOR, PUNCTUATOR
+};*/
+
+enum TokenType
+{
+	LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, LEFT_BRACK, RIGHT_BRACK,
+	COMMA, DOT, SEMICOLON, COLON, QUOTE,
+
+	OPERATOR,
+	//ADD_OPERATOR,		// + - OR
+	//MULTIPLY_OPERATOR	// * / AND DIV MOD
+	//RELATION_OPERATOR	// > < 
+
+	//EQUAL, EQUAL_EQUAL,
+	//GREATER, GREATER_EQUAL,
+	//LESS, LESS_EQUAL,
+
+	IDENTIFIER, STR, NUMBER,
+
+	VAR, FUNC, IF, ELSE, ELIF, AND, OR, NOT, PRINT, TRUE, FALSE, RETURN,
+
+	END_OF_FILE,
+	UNDEF // ??
+};
+
+struct Token
+{
+	TokenType type;
+	std::string lexeme;
+
+	Token(TokenType t, std::string l)
+	{
+		type = t;
+		lexeme = l;
+	}
+};
 
 enum LexMode
 {
--- SequiLanguage/src/parser.h
+++ SequiLanguage/src/parser.h
@@ -3,18 +3,44 @@
 
 #include <vector>
 
-#include "token.h"
+#include "lex.h"
 #include "statement.h"
 
 
-std::vector<Statement> parse(std::vector<Token>)
+struct Node
+{
+	
+};
+
+struct BinaryOp : public Node
+{
+	Node* left, right;
+	Token op;
+};
+
+struct Num : public Node
+{
+	Token number_token;
+};
+
+
+std::vector<Statement> parse(std::vector<Token> tokens)
 {
 	std::vector<Statement> statements;
 
+	int i = 0;
 	while (true)
 	{
-
+		if (tokens.at(i).type == LEFT_BRACE)
+			;
 	}
 	
 	return statements;
+}
+
+
+// TODO: handle errors
+void error(const char*)
+{
+	
 }
